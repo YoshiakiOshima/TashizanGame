80bcff05c955b9475cb44daff6a7b4cec0378a37
OSHIMAYOSHIAKI <oshimayoshiaki@OSHIMAYOSHIAKI-no-MacBook-Air.local>
2013-10-04 12:48:34 +0900

    Initial Commit

---
+++ hitasuraASHI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hitasuraASHI
+//
+//  Created by OSHIMAYOSHIAKI on 2013/10/04.
+//  Copyright (c) 2013年 OSHIMAYOSHIAKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
